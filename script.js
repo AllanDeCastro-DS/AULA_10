e6ae3f2455c46a51d105d63f7015cfbc3815c19f
Allan de Castro <116167880+AllanDeCastro-DS@users.noreply.github.com>
2024-09-27 12:09:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Allan";
+let sobrenome = "Castro";
+let idade = 17;
+
+console.log(
+  "Meu nome é " + nome + " " + sobrenome + " e minha idade é " + idade
+);
+
+let mensagem = `Meu nome é ${nome} de ${sobrenome} e tenho ${idade} anos`;
+
+console.log(mensagem);
